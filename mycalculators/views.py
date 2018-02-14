1e7740b22fb948bfb6f50f02225c8d273bed88c3
ncoll943 <ncoll943@students.bju.edu>
2018-02-14 17:00:33 -0500

    Adding initial views

--- mycalculators/views.py
+++ mycalculators/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, 'website/index.html', {'homeMenuActive': True})
+
+def login(request):
+    return render(request, 'website/index.html', {'homeMenuActive': True})
+
+def page(request):
+    return render(request, 'website/index.html', {'homeMenuActive': True})
